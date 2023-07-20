a35b48f82c9e697ae3300d06914666a410dc0a40
Jacobf24 <mp427725@gmail.com>
2023-07-20 10:56:07 -0600

    did excersizes

---
+++ index.js/script.js
@@ -0,0 +1,14 @@
+//Excersizes//
+let a = 5;
+b = 2;
+
+let add = a + b;
+let sub = a - b;
+let mul = a * b;
+let div = a / b;
+console.log(add, sub, mul, div);
+
+let dividend = 17;
+divisor = 3;
+let remainder = dividend % divisor;
+console.log(remainder);
